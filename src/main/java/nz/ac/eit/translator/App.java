d93a2118517cb6b2f5e4b639edb223fbe5d5b447
Dylan Williams <jexla@live.com>
2020-08-17 15:14:42 +1200

    REQ1 - Select number

--- src/main/java/nz/ac/eit/translator/App.java
+++ src/main/java/nz/ac/eit/translator/App.java
@@ -12,6 +12,19 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "What is the number to translate?" );
+        Scanner scanner=new Scanner(System.in);
+        String numberAsString=scanner.nextLine();
+
+        Integer number=null;
+        try {
+            number = Integer.parseInt(numberAsString);
+            System.out.println(number);
+        }
+        catch (NumberFormatException nfe) {
+            System.out.println("Please enter ONLY a numeric value");
+            System.exit(0);
+        }
 
 
 
